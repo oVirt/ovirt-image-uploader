4c5a4fb7390727a1f8f0664b2496dd7ce38b1819
Sandro Bonazzola <sbonazzo@redhat.com>
2015-08-11 08:51:52 +0200

    automation: add git hash to the build
    - add git hash to the build
    - avoid to use legacy make targets
    
    Change-Id: I080898023c62abda993c6003a969270eb3019881
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    (cherry picked from commit bf2d5096e437ba70a92d7a47231cbb0f64074dc4)

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -2,9 +2,20 @@
 [[ -d exported-artifacts ]] \
 || mkdir -p exported-artifacts
 
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+
+SUFFIX=".git$(git rev-parse --short HEAD)"
+
 autoreconf -ivf
 ./configure
-make rpm
+make dist
+rpmbuild \
+    -D "_srcrpmdir $PWD/tmp.repos" \
+    -D "_topmdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
+    -ta ovirt-image-uploader-*.tar.gz
+
 mv *.tar.gz exported-artifacts
 find \
     "$PWD/tmp.repos" \
