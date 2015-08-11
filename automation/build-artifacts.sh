68467165fd3efbc59483ffb6f0134016a47d8b07
Sandro Bonazzola <sbonazzo@redhat.com>
2015-08-11 09:49:38 +0200

    automation: fix missing noarch in exported-artifacts
    Change-Id: I2af295ad022ff4a256a6d653a6982e9d445ee5dd
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    (cherry picked from commit 9fff0b4b0692df8d976c2c52c63cce1d88a60c6e)

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -11,8 +11,7 @@ autoreconf -ivf
 ./configure
 make dist
 rpmbuild \
-    -D "_srcrpmdir $PWD/tmp.repos" \
-    -D "_topmdir $PWD/tmp.repos" \
+    -D "_topdir $PWD/tmp.repos" \
     -D "release_suffix ${SUFFIX}" \
     -ta ovirt-image-uploader-*.tar.gz
 
